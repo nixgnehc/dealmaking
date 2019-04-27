ab501ab3cf7e58d1fcfa57310aa0287e5e1447c8
成鑫 <chengxin@zbj.com>
2019-04-27 12:31:44 +0800

    新增redis配置

--- src/main/java/cn/dealmaking/config/JedisConfig.java
+++ src/main/java/cn/dealmaking/config/JedisConfig.java
@@ -1,10 +1,12 @@
 package cn.dealmaking.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisCluster;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,13 +20,19 @@ import java.util.Set;
 @Component
 public class JedisConfig {
 
+    @Value("${redis.config.connect}")
+    private String redisConnect;
 
     @Bean
     public JedisCluster jedisCluster() {
 
         Set<HostAndPort> jedisClusterNodes = new HashSet<HostAndPort>();
-//        jedisClusterNodes.add(new HostAndPort("10.200.70.7", 6380));
-        jedisClusterNodes.add(new HostAndPort("127.0.0.1", 8001));
+
+        Arrays.asList(redisConnect.split(",")).stream().forEach(node->{
+            String [] nodeArr = node.split(":");
+            jedisClusterNodes.add(new HostAndPort(nodeArr[0], Integer.parseInt(nodeArr[1])));
+        });
+
         JedisCluster jedisCluster = new JedisCluster(jedisClusterNodes);
         return jedisCluster;
     }
